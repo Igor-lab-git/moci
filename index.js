69486ff46b5cc8da07e13a9119d1640c686277e4
Igor <igorkost142@gmail.com>
2025-04-11 09:30:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#input');
+const div = document.querySelector('#div')
+
+
+
+btn.addEventListener('click', () => {
+
+    const input = document.querySelector('#input').value;
+
+    div.innerHTML += input
+})
